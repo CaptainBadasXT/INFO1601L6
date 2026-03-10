f4a4cbedf7b49cc1f0fa3484622fc315074eb3c7
CaptainBadasXT <kyjuanjulien76@gmail.com>
2026-03-09 20:08:42 -0400

    Change No.2

--- public/script.js
+++ public/script.js
@@ -43,3 +43,35 @@ console.log(window.sally === sally); // false
 var document = 'hello this is my variable';
 console.log(document); // gives the DOM object instead
 console.log(window.document === document); // true because declaration ignored
+
+function myFun() {
+  window.alert('hello');
+}
+
+function myFun2() {
+  console.log('myFun2 called');
+}
+
+// receives the event parameter from addEventListener callback
+function logEventType(event) {
+  console.log(event.type);
+}
+
+function loaded() {
+  console.log('img loaded');
+}
+
+const myBtn = document.querySelector('#myBtn');
+if (myBtn) {
+  // attach myFun2 in addition to myFun to the click event of myBtn
+  myBtn.addEventListener('click', myFun2);
+
+  // There are other events such as mouseover and mouseout
+  myBtn.addEventListener('mouseover', logEventType); // logs 'mouseover'
+  myBtn.addEventListener('mouseout', logEventType); // logs 'mouseout'
+}
+
+const cageImg = document.querySelector('#cageImg');
+if (cageImg) {
+  cageImg.addEventListener('load', loaded);
+}
